6b97c966b45e93346d1965df8433a0f5035bf89d
mazen546 <90868592+mazen546@users.noreply.github.com>
2025-03-06 01:32:47 +0200

    Apps Sections Completed

--- src/pages/Kanban.tsx
+++ src/pages/Kanban.tsx
@@ -1,7 +1,32 @@
-import React from "react";
-
+import { Header } from "../components";
+import {
+  KanbanComponent,
+  ColumnsDirective,
+  ColumnDirective,
+} from "@syncfusion/ej2-react-kanban";
+import { kanbanData, kanbanGrid } from "../data/dummy";
 const Kanban = () => {
-  return <div>Kanban</div>;
+  return (
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Kanban" />
+      <KanbanComponent
+        dataSource={kanbanData}
+        id="kanban"
+        keyField="Status"
+        cardSettings={{
+          contentField: "Summary",
+          headerField: "Id",
+        }}
+        swimlaneSettings={{ keyField: "Assignee" }}
+      >
+        <ColumnsDirective>
+          {kanbanGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+      </KanbanComponent>
+    </div>
+  );
 };
 
 export default Kanban;
